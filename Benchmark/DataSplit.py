d2e826eaf8f7d0720a276aace5a0774da56e3eb9
Sooyyoungg <43199011+Sooyyoungg@users.noreply.github.com>
2022-03-24 17:57:08 +0900

    b0 as input

--- Benchmark/DataSplit.py
+++ Benchmark/DataSplit.py
@@ -24,10 +24,12 @@ class DataSplit(Dataset):
         for i in range(len(self.data_csv)):
             sub = self.data_csv.iloc[i][1]
 
-            # Structure & diffusion-weighted image & Gradient
-            struct = np.load(self.data_dir + '/' + sub + '.T1.npy')  # (64, 64, 64)
-            dwi_raw = np.load(self.data_dir + '/' + sub + '.dwi.npy')  # (64, 64, 64, 103)
-            dwi_total = np.transpose(dwi_raw, (3, 0, 1, 2))  # (103, 64, 64, 64)
+            # Structure & b0 & diffusion-weighted image & Gradient
+            struct = np.load(self.data_dir + '/' + sub + '.T1.npy')    # (64, 64, 64)
+            b0_raw = np.load(self.data_dir + '/' + sub + '.b0.npy')    # (64, 64, 64, 7)
+            b0 = np.transpose(b0_raw, (3, 0, 1, 2))                    # (7, 64, 64, 64)
+            dwi_raw = np.load(self.data_dir + '/' + sub + '.dwi.npy')  # (64, 64, 64, 96)
+            dwi_total = np.transpose(dwi_raw, (3, 0, 1, 2))            # (96, 64, 64, 64)
             grad_file = open(self.data_dir + '/' + sub + '.grad.b').read()
 
             # change grad file into numpy
@@ -37,16 +39,17 @@ class DataSplit(Dataset):
             for i in range(len(grad_n)):
                 one_grad = grad_n[i].split(' ')
                 gg.append([float(one_grad[0]), float(one_grad[1]), float(one_grad[2]), float(one_grad[3])])
-            grad_total = np.array(gg)  # (103, 4)
+            grad_total = np.array(gg)  # (96, 4)
 
-            for j in range(103):
+            for j in range(dwi_total.shape[0]):
+                input = np.concatenate((struct.reshape((1, 64, 64, 64)), b0), axis=0)  # (8, 64, 64, 64)
                 dwi = dwi_total[j, :, :, :]  # (64, 64, 64)
                 grad = grad_total[j]  # (4)
-                self.total_st.append(struct)
+                self.total_st.append(input)
                 self.total_dwi.append(dwi)
                 self.total_grad.append(grad)
 
-        self.total_st = np.array(self.total_st)  # (13184, 64, 64, 64)
+        self.total_st = np.array(self.total_st)  # (13184, 8, 64, 64, 64)
         self.total_dwi = np.array(self.total_dwi)  # (13184, 64, 64, 64)
         self.total_grad = np.array(self.total_grad)  # (13184, 4)
 
@@ -54,19 +57,19 @@ class DataSplit(Dataset):
         return len(self.total_dwi)
 
     def __getitem__(self, index):
-        struct = self.total_st[index]
+        input = self.total_st[index]
         dwi = self.total_dwi[index]
         grad = self.total_grad[index]
 
         # Transform
         if self.do_transform is not None:
-            struct = self.transform(struct)
+            input = self.transform(input)
             dwi = self.transform(dwi)
             # grad = self.transform(grad)
 
         # Reshape
-        struct = struct.reshape((1, 64, 64, 64))
+        # input = input.reshape((1, 64, 64, 64))
         dwi = dwi.reshape((1, 64, 64, 64))
         # grad = grad.reshape((1, 4))
 
-        return {"t1": struct, "dwi": dwi, "cond": grad}
+        return {"t1": input, "dwi": dwi, "cond": grad}

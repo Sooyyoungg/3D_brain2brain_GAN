4c9ccdadc7378ac5f685ee9e2681f4a0036f8380
Sooyyoungg <43199011+Sooyyoungg@users.noreply.github.com>
2022-05-25 19:05:13 +0900

    total data

--- U-Net/train.py
+++ U-Net/train.py
@@ -2,53 +2,12 @@
 
 ## Parser 생성하기
 parser = argparse.ArgumentParser()
-
-parser.add_argument("--train_continue", default="on", choices=["on", "off"], type=str, dest="train_continue")
-parser.add_argument("--which_epoch", default=0, type=int, help="select which epoch to load")
-parser.add_argument("--test_save", default="off", choices=["on","off"],type=str, dest="test_save")
-
-parser.add_argument("--lr", default=2e-4, type=float, dest='lr', help="initial learning rate for adam")
-parser.add_argument('--lr_policy', type=str, default='lambda', help='learning rate policy: lambda|step|plateau|cosine')
-parser.add_argument('--niter', type=int, default=200, help='# of iter at starting learning rate')
-parser.add_argument('--niter_decay', type=int, default=200, help='# of iter to linearly decay learning rate to zero')
-
-
-parser.add_argument("--batch_size", default=16, type=int, dest="batch_size")
-parser.add_argument("--num_epoch", default=100, type=int, dest="num_epoch")
-parser.add_argument("--num_workers", default=16, type=int, dest="num_workers")
-
-# Newly added parameters
-parser.add_argument("--target", default='mri_info_manufacturersmn', type=str, dest="target")
-parser.add_argument("--resize",default=96, required=False,type=int, help='resized to volume having the size of the argument')
-
-parser.add_argument("--ckpt_dir", default='/home/connectome/junb/GANBERT/t1_harmonization/3D-cycleGAN/checkpoint', type=str, dest="ckpt_dir")
-parser.add_argument("--log_dir", default='/home/connectome/junb/GANBERT/t1_harmonization/3D-cycleGAN/log', type=str, dest="log_dir")
-parser.add_argument("--result_dir", default='/home/connectome/junb/GANBERT/t1_harmonization/3D-cycleGAN/result', type=str, dest="result_dir")
-parser.add_argument("--gpus", type=int, nargs='+',required=True, dest="gpus_list", help='')
-
-parser.add_argument("--task", default="cyclegan", choices=['cyclegan'], type=str, dest="task")
-parser.add_argument("--which_model_netG", default="unet_128", choices=['resnet_9blocks','resnet_6blocks','unet_custom','unet_128','unet_256','Dynet'], type=str, dest="which_model_netG")
-parser.add_argument("--which_model_netD", default='basic', choices=['basic','n_layers','pixel'], type=str, dest="which_model_netD")
-
-parser.add_argument('--opts', nargs='+', default=['direction', 0], dest='opts')
-
-parser.add_argument("--nch", default=1, type=int, dest="nch")
-parser.add_argument("--nker", default=64, type=int, dest="nker")
-
-parser.add_argument("--wgt_cycle", default=1e1, type=float, dest="wgt_cycle")
-parser.add_argument("--wgt_ident", default=5e-1, type=float, dest="wgt_ident")
-parser.add_argument("--norm", default='inorm', type=str, dest="norm")
-
-parser.add_argument("--network", default="CycleGAN", choices=['DCGAN', 'pix2pix', 'CycleGAN'], type=str, dest="network")
-parser.add_argument("--learning_type", default="plain", choices=["plain", "residual"], type=str, dest="learning_type")
-parser.add_argument("--d_loss", default="BCE", choices=["BCE", "wgangp"], type=str, dest="d_loss")
-
 args = parser.parse_args()
 
 device = torch.device('cuda:'+str(Config.gpu[0]) if torch.cuda.is_available() else 'cpu')
 print(device)
 
-### Data Loader
+## Data Loader
 config = Config()
 train_csv = pd.read_csv('/home/connectome/conmaster/Projects/Image_Translation/data_processing/sample_train.csv', header=None)
 val_csv = pd.read_csv('/home/connectome/conmaster/Projects/Image_Translation/data_processing/sample_val.csv', header=None)
@@ -72,3 +31,5 @@ data_loader_val = torch.utils.data.DataLoader(val_data, batch_size=config.batch_
 # 412 58
 print(len(data_loader_train), len(data_loader_val))
 
+## Model
+net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
